038049c7e9382d6f187ae68215b53ea1b3f3ee5b
Rustam Kashapov <hardtechnik@yandex.ru>
2019-08-05 14:58:32 +0300

    use inmemory database for testing

--- src/phonerepair/config/ci.py
+++ src/phonerepair/config/ci.py
@@ -3,3 +3,10 @@ from .base import *
 CI = True
 DOMAIN = 'test.server'
 ALLOWED_HOSTS = [DOMAIN]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '::memory::',
+    }
+}
