369e1eba626fec6677a5cd1eac86f5026b2d5261
Rustam Kashapov <hardtechnik@yandex.ru>
2019-08-09 18:57:11 +0300

    removed s3 endpoint from csp headers

--- src/core/middleware.py
+++ src/core/middleware.py
@@ -7,7 +7,7 @@ def security_headers(get_response):
         response['Strict-Transport-Security'] = \
             'max-age=31536000; includeSubDomains'
         response['Content-Security-Policy'] = \
-            f'default-src {settings.S3_ENDPOINT} https://mc.yandex.ru ' \
+            f'default-src https://mc.yandex.ru ' \
             f'\'self\' \'unsafe-inline\' data:'
         response['X-Content-Type-Options'] = 'nosniff'
         response['Referrer-Policy'] = 'no-referrer-when-downgrade'
