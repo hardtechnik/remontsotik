1c21a83f2ad7b15cea992d7aaec03716f17d14fa
Rustam Kashapov <hardtechnik@yandex.ru>
2019-08-10 00:16:50 +0300

    deduplicated signal handlers

--- src/core/handlers.py
+++ src/core/handlers.py
@@ -5,13 +5,13 @@ from .models import Ticket
 from .tasks import send_new_ticket_created_email, send_status_email
 
 
-@receiver(post_init, sender=Ticket)
+@receiver(post_init, sender=Ticket, dispatch_uid='set_previous_state_id')
 def set_previous_state_id(instance, **kwargs):
     instance.previous_status_id = getattr(instance, 'status_id', None) \
         if instance.id else None
 
 
-@receiver(post_save, sender=Ticket)
+@receiver(post_save, sender=Ticket, dispatch_uid="send_status_update")
 def send_status_update(instance, created, **kwargs):
     if created:
         send_new_ticket_created_email.delay(instance.id)
