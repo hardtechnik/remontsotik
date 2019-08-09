f744599dbb923d2e74091e551a9f0c875969f1d4
Rustam Kashapov <hardtechnik@yandex.ru>
2019-08-09 17:25:34 +0300

    inline image in email template

--- src/environment.py
+++ src/environment.py
@@ -1,13 +1,23 @@
+import base64
+
+from django.contrib.staticfiles import finders
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
 
 from jinja2 import Environment
 
 
+def inline(src, mime):
+    with open(finders.find(src), 'rb') as f:
+        content = base64.b64encode(f.read())
+    return f'data:{mime};base64,{content}'
+
+
 def environment(**options):
     env = Environment(**options)
     env.globals.update({
         'static': staticfiles_storage.url,
+        'inline': inline,
         'url': reverse,
     })
     return env
