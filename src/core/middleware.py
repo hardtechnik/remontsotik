ee842211d9123681993362aa0153daed27b43c4e
Rustam Kashapov <hardtechnik@yandex.ru>
2019-08-24 01:04:14 +0300

    added vk like widget

--- src/core/middleware.py
+++ src/core/middleware.py
@@ -7,7 +7,10 @@ def security_headers(get_response):
         response['Strict-Transport-Security'] = \
             'max-age=31536000; includeSubDomains'
         response['Content-Security-Policy'] = \
-            f'default-src {settings.S3_ENDPOINT} https://mc.yandex.ru ' \
+            f'default-src ' \
+            f'{settings.S3_ENDPOINT} ' \
+            f'https://mc.yandex.ru ' \
+            f'https://vk.com' \
             f'\'self\' \'unsafe-inline\' data:'
         response['X-Content-Type-Options'] = 'nosniff'
         response['Referrer-Policy'] = 'no-referrer-when-downgrade'
