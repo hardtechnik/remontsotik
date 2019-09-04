8dacad2bde2ea7a49fb2357fc86db1683b3ecdba
Rustam Kashapov <hardtechnik@yandex.ru>
2019-09-04 10:50:59 +0300

    disabled captcha check on CI

--- src/core/forms.py
+++ src/core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 
 from captcha.fields import ReCaptchaField, ReCaptchaV3
 
@@ -9,6 +10,12 @@ class TicketForm(forms.ModelForm):
     captcha = ReCaptchaField(widget=ReCaptchaV3(api_params={
         'hl': 'ru',
     }))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if settings.CI:
+            self.errors.pop('captcha', None)
+        return cleaned_data
 
     class Meta:
         exclude = (
